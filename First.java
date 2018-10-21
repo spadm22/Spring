0d7004aa9a281b161125ce83af390757ee0c57fc
Padmanabha Sahu <39075856+spadm22@users.noreply.github.com>
2018-10-21 11:03:03 +0530

    Raja method added

--- First.java
+++ First.java
@@ -2,5 +2,10 @@
 class First{
   public static void main(String args[]){
     System.out.println("My First");
+    First f = new First();
+    f.m1("Raja");
+  }
+  public void m1(String name){
+    System.out.println(name);
   }
 }
